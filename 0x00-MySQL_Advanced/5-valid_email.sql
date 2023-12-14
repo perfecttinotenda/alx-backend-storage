d2ea7e34873aae0caa94fc50c034d392e84670e5
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-12-14 21:42:53 +0200

    task5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,6 @@
   
--- Task 5: Email validation to sent - creates a trigger that resets the attribute valid_email
--- only when the email has been changed
+-- Task-5: Email validation to be sent - will create a trigger that resets the attr valid_email
+-- only if the email has been changed
 DELIMITER |
 CREATE TRIGGER email_bool BEFORE UPDATE ON users
 FOR EACH ROW
